80c6fcd0981124c247ff7baaa1925d1580621dc5
Ananda <anandaguneratne@gmail.com>
2020-06-29 21:59:25 -0500

    fixed physics frame-dependency bug

--- Cells/Flagellum.gd
+++ Cells/Flagellum.gd
@@ -5,6 +5,6 @@ onready var player: RigidBody2D = get_parent()
 func _physics_process(delta):
 
 	var relative_position = global_transform.get_origin() - player.global_transform.get_origin()
-	var relative_force = Vector2.UP.rotated(global_transform.get_rotation()) * 2
+	var relative_force = Vector2.UP.rotated(global_transform.get_rotation()) * 30
 
-	player.apply_impulse(relative_position, relative_force)
+	player.apply_impulse(relative_position, relative_force * delta)
